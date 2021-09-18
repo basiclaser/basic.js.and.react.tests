6593c47eb363d229066e53f99268936f7c50b8e0
z-memory <dont@forgetmy.email>
2021-09-18 11:14:50 +0200

    add jest js test

---
+++ src/utilities.spec.js
@@ -0,0 +1,18 @@
+import {add} from './utilities.js'
+
+describe("utilities", () => {
+    describe("add", () => {
+     // assertions / expectations
+        it("should add two numbers", () => {
+            expect(add(1, 2)).toBe(3)
+        })   
+    })
+}) 
+
+// JEST - TEST RUNNER 
+// JEST - describe / it / expect / 
+
+// react-testing-library - test render, expect 
+// react-related features - rendering components and checking them 
+
+
